8864faf1eb494a84908bdb57aad02d0b81d1547c
Shaily Fadadu <shailifadadu@gmail.com>
2025-02-25 22:29:34 +0530

    Added DashboardBlocks

---
+++ app/components/DashboardBlocks.tsx
@@ -0,0 +1,112 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+import { prisma } from "../utils/db";
+import { requiredUser } from "../utils/hooks";
+
+async function getData(userId: string) {
+  //run multiple prisma queries
+  const [data, openInvoices, paidInvoices] = await Promise.all([
+    prisma.invoice.findMany({
+      where: {
+        userId: userId,
+      },
+      select: {
+        total: true,
+      },
+    }),
+
+    prisma.invoice.findMany({
+      where: {
+        userId: userId,
+        status: "PENDING",
+      },
+      select: {
+        id: true,
+      },
+    }),
+
+    prisma.invoice.findMany({
+      where: {
+        userId: userId,
+        status: "PAID",
+      },
+      select: {
+        id: true,
+      },
+    }),
+  ]);
+
+  return {
+    data,
+    openInvoices,
+    paidInvoices,
+  };
+}
+
+export async function DashboardBlocks() {
+  const session = await requiredUser();
+  //get the data
+  const { data, openInvoices, paidInvoices } = await getData(
+    session.user?.id as string
+  );
+
+  return (
+    <div>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+          <DollarSign className="size-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <h2 className="text-2xl font-bold">
+            {data.reduce((acc, invoice) => acc + invoice.total, 0)}
+          </h2>
+          <p className="text-xs text-muted-foreground">
+            Based on the last 30 days
+          </p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">
+            Total Invoices Issued
+          </CardTitle>
+          <Users className="size-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <h2 className="text-2xl font-bold">+{data.length}</h2>
+          <p className="text-xs text-muted-foreground">
+            Total Invoices Issued!
+          </p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Paid Invoices</CardTitle>
+          <CreditCard className="size-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <h2 className="text-2xl font-bold">+{paidInvoices.length}</h2>
+          <p className="text-xs text-muted-foreground">
+            Total Invoices which have been paid!
+          </p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Open Invoices</CardTitle>
+          <Activity className="size-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <h2 className="text-2xl font-bold">+{openInvoices.length}</h2>
+          <p className="text-xs text-muted-foreground">
+            Invoices which haven't been paid!
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,10 @@
-import React from "react";
-import { requiredUser } from "../utils/hooks";
-import { signOut } from "../utils/auth";
+import { DashboardBlocks } from "../components/DashboardBlocks";
 
 //async is allowed bcoz it's server component
 export default async function DashboardRoute() {
-  const session = await requiredUser();
-
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
-    </div>
+    <>
+      <DashboardBlocks />
+    </>
   );
 }
