51bb73c40d8934313bcd206239f1896db1c7ef31
Shaily Fadadu <shailifadadu@gmail.com>
2025-02-27 18:19:16 +0530

    Added Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
-  return <div>hi there</div>;
+  return (
+    <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
